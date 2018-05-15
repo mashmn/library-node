84e94e3339160d4172161904a80ae7ea121fdc1d
mashmn <borne.god@gmail.com>
2018-05-14 22:39:23 -0400

    Leveraging ejs to change UI

--- app.js
+++ app.js
@@ -19,7 +19,14 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname, '/views/index.html'));
-    res.render('index', {list: ['a', 'b'], title: 'Library'});
+    res.render(
+        'index', 
+        {
+            nav: [{link:'/books', title:'Books'}, 
+                {link:'/authors', title:'Authors'}],
+            title: 'Library'
+        }
+    );
 });
 
 app.listen(port, () => {
