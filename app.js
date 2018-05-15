69d6328bb98073078c192c1b94ead6b11fd6e744
mashmn <borne.god@gmail.com>
2018-05-14 22:27:38 -0400

    Library app with basic view

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+// TO DEBUG
+// set DEBUG=*,-not_this
+// $env:DEBUG = "*,-not_this"
+// node app.js
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000; 
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    // res.sendFile(path.join(__dirname, '/views/index.html'));
+    res.render('index', {list: ['a', 'b'], title: 'Library'});
+});
+
+app.listen(port, () => {
+    debug(`listening at port ${chalk.green(port)}`);
+});
