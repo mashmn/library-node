95e95e55ef0c81ceabafa36fe23135bb2e472b8c
mashmn <borne.god@gmail.com>
2018-05-14 23:03:13 -0400

    routing

--- app.js
+++ app.js
@@ -10,13 +10,48 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000; 
+const bookRouter = express.Router();
+
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')));
 app.set('views', './src/views');
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+const books = [
+    {
+        title: 'War and Peace',
+        genre: 'History',
+        author: 'Lev',
+        read: false
+    },
+    {
+        title: 'Men and Women',
+        genre: 'Life',
+        author: 'Chilly',
+        read: false
+    }
+];
+
+bookRouter.route('/')
+    .get((req, res) => {
+        res.render('books', 
+        {
+            nav: [{link:'/books', title:'Books'}, 
+                {link:'/authors', title:'Authors'}],
+            title: 'Library',
+            books
+        });
+    });
+
+bookRouter.route('/single')
+    .get((req, res) => {
+        res.send('hello single books');
+    });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
     // res.sendFile(path.join(__dirname, '/views/index.html'));
     res.render(
