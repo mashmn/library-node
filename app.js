9258e66773179f40a127570af495c3ae00c7dff5
mashmn <borne.god@gmail.com>
2018-05-28 13:35:49 -0400

    Azure Database configuration, config file not included

--- app.js
+++ app.js
@@ -7,11 +7,35 @@ const debug = require('debug')('app');
 // node app.js
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
+
+const config = require('./config');
+
+// Create a "config.js" file and add the database configuration.
+// In this case, I have used Azure SQL server.
+// 
+// const config = {
+//     user: 'user',
+//     password: 'password123',
+//     server: 'library.database.windows.net',
+//     database: 'Library',
+//     options: {
+//         encrypt: true
+//     }
+// }
+// module.exports = config;
+
+sql.connect(config).catch(err => debug(err));
 
 app.use(morgan('tiny'));
+app.use((req, res, next) => {
+    debug('my middleware');
+    next();
+});
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
@@ -19,7 +43,12 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist/')
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-const bookRouter = require('./src/routes/bookRoutes');
+const nav = [
+    {link:'/books', title:'Book'}, 
+    {link:'/authors', title:'Author'}
+]
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use('/books', bookRouter);
 app.get('/', (req, res) => {
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,44 +1,59 @@
 const express = require('express');
 const bookRouter = express.Router();
+const sql = require('mssql');
+const debug = require('debug')('app:bookRoutes');
 
-const books = [
-    {
-        title: 'War and Peace',
-        genre: 'History',
-        author: 'Lev',
-        read: false
-    },
-    {
-        title: 'Men and Women',
-        genre: 'Life',
-        author: 'Chilly',
-        read: false
-    }
-];
+function router(nav) {
+    const books = [
+        {
+            title: 'War and Peace',
+            genre: 'History',
+            author: 'Lev',
+            read: false
+        },
+        {
+            title: 'Men and Women',
+            genre: 'Life',
+            author: 'Chilly',
+            read: false
+        }
+    ];
+    
+    bookRouter.route('/')
+        .get((req, res) => {
+            (async function query() {
+                const request = new sql.Request();
+                const {recordset} = await request.query('select * from books');
+                // debug(result);
+                res.render('bookListView',
+                    {
+                        nav,
+                        title: 'Library',
+                        books: recordset
+                    }
+                );
+            }());            
+        });
+    
+    bookRouter.route('/:id')
+        .get((req, res) => {
+            (async function query(){
+                const id = req.params.id;
+                const request = new sql.Request();
+                const {recordset} = 
+                    await request.input('id', sql.Int, id)
+                    .query('select * from books where id = @id');
+                debug(recordset);
+                res.render('bookView',
+                    {
+                        nav,
+                        title: 'Library',
+                        book: recordset[0]
+                    }
+                );
+            }());
+        });
+    return bookRouter;
+}
 
-bookRouter.route('/')
-    .get((req, res) => {
-        res.render('bookListView',
-            {
-                nav: [{link:'/books', title:'Books'}, 
-                    {link:'/authors', title:'Authors'}],
-                title: 'Library',
-                books
-            }
-        );
-    });
-
-bookRouter.route('/:id')
-    .get((req, res) => {
-        const id = req.params.id;
-        res.render('bookView',
-            {
-                nav: [{link:'/books', title:'Books'}, 
-                    {link:'/authors', title:'Authors'}],
-                title: 'Library',
-                book: books[id]
-            }
-        );
-    });
-
-module.exports = bookRouter;
+module.exports = router;
